9c556d8609513d532d51522a9b32ccab801bccdd
Hirise101 <2770529789@qq.com>
2023-04-07 21:30:16 +0800

    登录退出

--- src/main/java/com/jyq/reggie/controller/EmployeeController.java
+++ src/main/java/com/jyq/reggie/controller/EmployeeController.java
@@ -8,9 +8,10 @@ import com.jyq.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
@@ -29,11 +30,13 @@ public class EmployeeController {
      */
     @GetMapping("/page")
     public R<Page> page(int page,int pageSize,String name){
+        //控制台输出日志
+        log.info("执行分页查询:page={},pageSize={},name={}",page,pageSize,name);
         //构造分页构造器
         Page pageInfo=new Page(page,pageSize);
         //构造条件过滤器
         LambdaQueryWrapper<Employee> queryWrapper=new LambdaQueryWrapper();
-        //添加过滤条件
+        //添加过滤条件,这里使用like作为模糊查询
         queryWrapper.like(StringUtils.isNotEmpty(name),Employee::getUsername,name);
         //添加排序条件
         queryWrapper.orderByDesc(Employee::getUpdateTime);
@@ -42,6 +45,41 @@ public class EmployeeController {
 
         return R.success(pageInfo);
     }
+
+    @PostMapping("/login")
+     public R<Employee> login(HttpServletRequest request,@RequestBody Employee employee){
+        log.info("登录账户");
+        //获得密码并进行md5加密
+        String password= employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+        //根据username获得employee对象
+        LambdaQueryWrapper<Employee> queryWrapper=new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername,employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+
+        //判断用户能否登录
+        if(emp==null){
+            return R.error("用户不存在，登陆失败");
+        }
+        if(!emp.getPassword().equals(password)){
+            return R.error("密码错误，登陆失败");
+        }
+        if(emp.getStatus()==0){
+            return R.error("账号已禁用，登陆失败");
+        }
+        //在session中设置属性为employee，值为id
+        request.getSession().setAttribute("employee",emp.getId());
+        return R.success(emp);
+     }
+     @PostMapping("/logout")
+     public R<String> logout(HttpServletRequest request){
+        log.info("账户退出");
+        //清理session中的属性值
+         request.getSession().removeAttribute("employee");
+        return R.success("退出成功！");
+     }
+
 
 
 }
